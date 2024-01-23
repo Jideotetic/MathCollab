a6080385c48991c69df269193f65abe76d113963
Jideotetic <jideotetic@gmail.com>
2024-01-23 10:45:33 +0100

    Added the dashboard

--- src/routes/DashboardIndex.tsx
+++ src/routes/DashboardIndex.tsx
@@ -2,6 +2,7 @@ import homeUrl from "../assets/home.svg";
 import notificationUrl from "../assets/notification.svg";
 import userImageUrl from "../assets/user-image.png";
 import arrowRightIconUrl from "../assets/Icon.svg";
+// import { Bars3Icon } from "@heroicons/react/24/solid";
 
 export default function DashboardIndex() {
   return (
@@ -36,8 +37,34 @@ export default function DashboardIndex() {
       <div className="flex h-16 items-center border-2 border-neutral-200 px-4">
         Content
       </div>
-      <div className="flex flex-grow items-center justify-center border-2 border-neutral-200">
-        Content
+      <div className="flex flex-grow items-center justify-center border-2 border-neutral-200 text-center">
+        <div className="flex max-w-[551px] flex-col items-center justify-center gap-8 border-2 border-red-200">
+          <div></div>
+          <div className="flex max-w-[365px] flex-col gap-8 border-2 border-blue-200">
+            <div>
+              <p className="text-xl font-semibold leading-[30px] text-neutral-700">
+                Start by creating or joining a class
+              </p>
+              <p className="text-base font-normal leading-normal text-neutral-500">
+                Create or join class to get started
+              </p>
+            </div>
+            <div>
+              <button
+                type="button"
+                className="w-[141px] rounded border border-slate-950 py-1.5 text-sm font-normal leading-[21px]"
+              >
+                Create a class
+              </button>
+              <button
+                type="button"
+                className="w-[141px] py-1.5 text-sm font-normal leading-[21px] text-orange-500 "
+              >
+                Join a class
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
